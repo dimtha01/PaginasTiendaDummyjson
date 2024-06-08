41b65cfd185fa563b58e5c20c75054a92bc6168b
ricar01010 <richarsonmartine@gmail.com>
2024-06-08 15:07:16 -0400

    Version 4

--- src/page/Tienda.jsx
+++ src/page/Tienda.jsx
@@ -1,34 +1,74 @@
 import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Boton from "../components/Boton";
-const API = 'https://dummyjson.com/products';
+const API = 'https://dummyjson.com/products?limit=10&skip=';
 
 const Tienda = () => {
     const [datos, setDatos] = useState([])
+    const [skip, setSkip] = useState(0);
+    const [total, setTotal] = useState(0);
     const getDatos = async () => {
+        const URI=API+skip
         try {
-            const response = await fetch(API);
+            const response = await fetch(URI);
             const data = await response.json();
             console.log(data)
             setDatos(data.products);
+            setTotal(data.total)
         } catch (error) {
             console.error(error)
         }
     };
     useEffect(() => {
         getDatos();
-    }, []);
+    }, [skip]);
     return (
         <>
             <div className="container">
                 <h1 className="text-center py-3 lead display-6">Todos los Productos <span class="badge rounded-pill text-bg-danger">{datos.length}</span></h1>
-                <Boton/>
+                <Boton />
                 <div className="row">
                     {datos && datos.map((item) => (
-                        
+
                         <Card key={item.id} item={item} />
                     ))}
                 </div>
+                <div className="d-flex justify-content-evenly align-content-center  text-black mb-3 p-2 alert alert-dark">
+                    <p className="lead m-0 text-light">Pagina {Math.floor(skip / 10) + 1} de {Math.ceil(total / 10)}</p>
+                    <nav aria-label="Page navigation example">
+                        <ul className="pagination m-0">
+                            <li className="page-item">
+                                <a className="page-link" href="#"
+                                    onClick={() => {
+                                        if (skip >= 10) {
+                                            setSkip(skip - 10);
+                                        }
+                                    }}
+                                >
+                                    &lt;&lt;
+                                </a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#">
+                                    {Math.floor(skip / 10) + 1}
+                                </a>
+                            </li>
+                            <li className="page-item">
+                                <a className="page-link" href="#"
+                                    onClick={() => {
+                                        if (skip + 10 < total) {
+                                            setSkip(skip + 10);
+                                        }
+                                    }}
+                                >
+                                    &gt;&gt;
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                </div>
+
             </div>
         </>
     )
